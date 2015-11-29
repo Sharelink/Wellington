7a6efa4d1becce48b76a911036472cefc0fb6bf1
AlexChow <cplover@live.com>
2015-11-29 13:56:58 +0800

    init commit

---
+++ src/Wellington/Controllers/ExeSharelinkController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TorontoWebsite.Controllers
+{
+    public class ExeSharelinkController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(string cmd)
+        {
+            string ua = Request.Headers["User-Agent"];
+            ua = ua.ToLower();
+            
+            if (ua.Contains("ios") || ua.Contains("iphone") || ua.Contains("ipad") || ua.Contains("ipod"))
+            {
+                NLog.LogManager.GetLogger("Request").Info("IOSExeSharelink");
+                return Redirect("~/IOSExeSharelink.html?cmd=" + cmd);
+            }
+            else if (ua.Contains("android"))
+            {
+                NLog.LogManager.GetLogger("Request").Info("AndroidExeSharelink");
+                return Redirect("~/AndroidExeSharelink.html?cmd=" + cmd);
+            }
+            else if(ua.Contains("windows mobile") || ua.Contains("windows phone"))
+            {
+                NLog.LogManager.GetLogger("Request").Info("WMExeSharelink");
+                return Redirect("~/WMExeSharelink.html?cmd=" + cmd);
+            }
+            else
+            {
+                return Redirect("~/");
+            }
+        }
+    }
+}
---
+++ src/Wellington/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using NLog;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TorontoWebsite
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+        public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+        {
+            // Setup configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath);
+            if (env.IsDevelopment())
+            {
+                builder.AddJsonFile("config.json");
+            }
+            else
+            {
+                builder.AddJsonFile("/etc/bahamut/website.json");
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //Log
+            var logConfig = new NLog.Config.LoggingConfiguration();
+            var fileTarget = new NLog.Targets.FileTarget();
+            fileTarget.FileName = Configuration["Data:Log:logFile"];
+            fileTarget.Name = "FileLogger";
+            fileTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message};${exception}";
+            logConfig.AddTarget(fileTarget);
+            logConfig.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, fileTarget));
+            if (env.IsDevelopment())
+            {
+                var consoleLogger = new NLog.Targets.ColoredConsoleTarget();
+                consoleLogger.Name = "ConsoleLogger";
+                consoleLogger.Layout = @"${date:format=HH\:mm\:ss} ${logger}:${message};${exception}";
+                logConfig.AddTarget(consoleLogger);
+                logConfig.LoggingRules.Add(new NLog.Config.LoggingRule("*", NLog.LogLevel.Debug, consoleLogger));
+            }
+            LogManager.Configuration = logConfig;
+
+            // Add the platform handler to the request pipeline.
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            LogManager.GetLogger("Website").Info("Started!");
+        }
+    }
+}
